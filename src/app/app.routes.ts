603a12c6b0def316ab033ffb3e872f99207a19c3
Francesca Piccitto <framari.p@gmail.com>
2024-06-13 11:54:51 +0200

    aggiunge i component header, footer e last-items

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LastItemsComponent } from './last-items/last-items.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'last', component: LastItemsComponent}
+];
---
+++ src/app/last-items/last-items.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-last-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './last-items.component.html',
+  styleUrl: './last-items.component.css'
+})
+export class LastItemsComponent implements OnInit{
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
