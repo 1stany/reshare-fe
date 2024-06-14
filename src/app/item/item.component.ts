e3b38799b0736afba078203b7afe7190cd108d3b
Francesca Piccitto <framari.p@gmail.com>
2024-06-14 13:01:38 +0200

    implementa bottoni indietro e scambia in item component

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Item, ItemService } from '../services/item.service';
 
 @Component({
   selector: 'app-item',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './item.component.html',
   styleUrl: './item.component.css'
 })
+
 export class ItemComponent implements OnInit{
+
   constructor(private itemService : ItemService, private route : ActivatedRoute) { }
   item : Item | undefined;
+
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get("id");
     if(id){
