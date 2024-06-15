9078c64c48f346113bebd3879b911ccb2426ae67
alessiorong <alessiorongione0598@gmail.com>
2024-06-15 15:33:58 +0200

    inizia implementazione form login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+
+interface LoginAnswer {
+  token : string;
+}
+
+
 
 @Component({
   selector: 'app-login',
@@ -10,6 +18,28 @@ import { RouterModule } from '@angular/router';
 })
 //ng add @angular/material
 // Angular Material per i componenti dell'interfaccia utente
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  email = '';
+  password = '';
+  token = '';
 
+  constructor(private http : HttpClient){}
+
+  ngOnInit(): void {}
+
+  onSubmit(){}
+
+  login() : void {
+    const user = { email: this.email, password: this.password};
+    this.http.post<LoginAnswer>('/api/login', user).subscribe(
+      (answer) => {
+        this.token = answer.token;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
+
+
