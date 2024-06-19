a28a4f49ed330ce4326bacc0aeaf70cc58faf2b2
alessiorong <alessiorongione0598@gmail.com>
2024-06-19 11:25:07 +0200

    inizia implementazione add-itemtype

--- src/app/add-itemtype/add-itemtype.component.ts
+++ src/app/add-itemtype/add-itemtype.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { ItemType } from '../model/itemtype.model';
+import { WishlistService } from '../services/wishlist.service';
 
 @Component({
   selector: 'app-add-itemtype',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-itemtype.component.html',
   styleUrl: './add-itemtype.component.css'
 })
-export class AddItemtypeComponent {
+export class AddItemtypeComponent implements OnInit {
+  typeForm!: FormGroup;
+
+
+
+constructor(private formBuilder : FormBuilder, private wishlistService: WishlistService){}
+  
+  
+  ngOnInit(): void {
+    this.typeForm = this.formBuilder.group({
+      name : ['', Validators.required],
+      description : ['', Validators.required],
+      categoryName : ['', Validators.required]
+    })
+  }
+
+  onSubmit(){
+
+  }
+  
 
 }
